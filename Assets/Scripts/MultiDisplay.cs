746c464ba837e9519344903d50edba60ac9ca3bd
Steve Gotzel <steve.gotzel@3m5.de>
2018-10-20 12:45:08 +0200

    Add MultiDisplay

---
+++ Assets/Scripts/MultiDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiDisplay : MonoBehaviour {
+    // Use this for initialization
+    void Start() {
+        Debug.Log("displays connected: " + Display.displays.Length);
+        for (int i = 0; i < Display.displays.Length; i++) {
+            Display.displays[i].Activate();
+        }   
+    }
+}
