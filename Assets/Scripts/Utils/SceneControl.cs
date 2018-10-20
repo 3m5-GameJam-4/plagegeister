c064fbd26c793525ab3e4129a68d8791b9ecf6a0
Lars Wobus <lars.wobus@web.de>
2018-10-20 15:01:19 +0200

    Add simple start scene to start/quit game

---
+++ Assets/Scripts/Utils/SceneControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+	public void LoadSceneByIndex(int sceneBuildIndex)
+	{
+		StartCoroutine(LoadSceneByIndexAsync(sceneBuildIndex));
+	}
+
+	public void LoadSceneByName(string sceneName)
+	{
+		StartCoroutine(LoadSceneByNameAsync(sceneName));
+	}
+	
+	private static IEnumerator LoadSceneByIndexAsync(int sceneBuildIndex)
+	{
+		var asyncLoad = SceneManager.LoadSceneAsync(sceneBuildIndex);
+
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+	}
+	
+	private static IEnumerator LoadSceneByNameAsync(string sceneName)
+	{
+		var asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+	}
+}
