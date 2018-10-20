051091a1db7239187ea0d049ce1abbb1f03a2b7a
Lars Wobus <lars.wobus@web.de>
2018-10-20 20:04:05 +0200

    Startscene can load Map and Audio at once

--- Assets/Scripts/Utils/SceneControl.cs
+++ Assets/Scripts/Utils/SceneControl.cs
@@ -1,37 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SceneControl : MonoBehaviour
 {
+	[SerializeField] private UnityEvent _onLoadFinished;
+
+	private List<AsyncOperation> _asyncOperations = new List<AsyncOperation>();
+	
 	public void LoadSceneByIndex(int sceneBuildIndex)
 	{
-		StartCoroutine(LoadSceneByIndexAsync(sceneBuildIndex));
+		StartCoroutine(LoadSceneByIndexAsync(sceneBuildIndex, LoadSceneMode.Single));
 	}
 
 	public void LoadSceneByName(string sceneName)
 	{
-		StartCoroutine(LoadSceneByNameAsync(sceneName));
+		StartCoroutine(LoadSceneByNameAsync(sceneName, LoadSceneMode.Single));
 	}
 	
-	private static IEnumerator LoadSceneByIndexAsync(int sceneBuildIndex)
+	public void LoadSceneAdditiveByIndex(int sceneBuildIndex)
 	{
-		var asyncLoad = SceneManager.LoadSceneAsync(sceneBuildIndex);
+		StartCoroutine(LoadSceneByIndexAsync(sceneBuildIndex, LoadSceneMode.Additive));
+	}
 
-		while (!asyncLoad.isDone)
-		{
-			yield return null;
-		}
+	public void LoadSceneAdditiveByName(string sceneName)
+	{
+		StartCoroutine(LoadSceneByNameAsync(sceneName, LoadSceneMode.Additive));
 	}
 	
-	private static IEnumerator LoadSceneByNameAsync(string sceneName)
+	private IEnumerator LoadSceneByIndexAsync(int sceneBuildIndex, LoadSceneMode loadSceneMode)
 	{
-		var asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-
-		while (!asyncLoad.isDone)
+		var asyncLoad = SceneManager.LoadSceneAsync(sceneBuildIndex, loadSceneMode);
+		asyncLoad.allowSceneActivation = false;
+		/*while (!asyncLoad.isDone)
 		{
 			yield return null;
+		}*/
+		
+		_asyncOperations.Add(asyncLoad);
+		_onLoadFinished.Invoke();
+		yield return null;
+	}
+	
+	private IEnumerator LoadSceneByNameAsync(string sceneName, LoadSceneMode loadSceneMode)
+	{
+		var asyncLoad = SceneManager.LoadSceneAsync(sceneName, loadSceneMode);
+		asyncLoad.allowSceneActivation = false;
+		/*while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}*/
+		
+		_asyncOperations.Add(asyncLoad);
+		_onLoadFinished.Invoke();
+		yield return null;
+	}
+
+
+	public void ActivateScenes()
+	{
+		foreach (var asyncOperation in _asyncOperations)
+		{
+			asyncOperation.allowSceneActivation = true;
 		}
+		_asyncOperations.Clear();
 	}
 }
---
+++ Assets/Scripts/Utils/SceneCounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SceneCounter : MonoBehaviour
+{
+	[SerializeField] private int _targetCount = 2;
+	[SerializeField] private UnityEvent _onCountReached;
+	private int _internalCount;
+
+	public void IncreaseCount(int value)
+	{
+		TargetCount += value;
+	} 
+	
+	private int TargetCount
+	{
+		get { return _internalCount; }
+		set
+		{
+			if (_internalCount == value)
+			{
+				return;
+			}
+			
+			_internalCount = value;
+			OnCountChanged();
+		}
+	}
+
+	private void OnCountChanged()
+	{
+		if (TargetCount == _targetCount)
+		{
+			_onCountReached.Invoke();
+		}
+	}
+}
