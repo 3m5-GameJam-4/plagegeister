8a4f51938f49813b89592daaa0d2abf0eee5df26
Gilbert Röhrbein <gilbert@ifsr.de>
2018-10-20 12:44:12 +0200

    MoveByForce: two swarms, InputManager two axis pairs, MoveByForce more configurable

--- Assets/Scripts/MoveByForce.cs
+++ Assets/Scripts/MoveByForce.cs
@@ -7,8 +7,11 @@ public class MoveByForce : MonoBehaviour
 {
     public Rigidbody2D rb;
 
+    public string horizontalAxis;
+    public string verticalAxis;
     public float thrust = 1f;
-
+    public ForceMode2D forceMode = ForceMode2D.Impulse;
+    
     void Start()
     {
         if (!rb) rb = GetComponent<Rigidbody2D>();
@@ -16,16 +19,11 @@ public class MoveByForce : MonoBehaviour
 
     void FixedUpdate()
     {
-        var hori = Input.GetAxis("Horizontal");
-        var vert = Input.GetAxis("Vertical");
+        var hori = Input.GetAxis(horizontalAxis);
+        var vert = Input.GetAxis(verticalAxis);
 
         var direction = new Vector2(hori, vert);
         
         rb.AddForce(direction * thrust, ForceMode2D.Impulse);
-    }
-
-    public void Foo()
-    {
-        Debug.Log("XXX");
     }
 }
