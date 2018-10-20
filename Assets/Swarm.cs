b908b660136e0cac63f7ef37e975dcb2e8b44058
Gilbert Röhrbein <gilbert@ifsr.de>
2018-10-20 13:29:12 +0200

    MoveByForce: add Player prefab, add Swarm script, update Swarm by Player

---
+++ Assets/Swarm.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarm : MonoBehaviour
+{
+    public Player player;
+
+    void Start()
+    {
+        if (!player) return;
+
+        var renderer = GetComponent<SpriteRenderer>();
+        if (renderer) updateRenderer(renderer);
+
+        var moveByForce = GetComponent<MoveByForce>();
+        if (moveByForce) updateMoveByForce(moveByForce);
+    }
+
+    private void updateRenderer(SpriteRenderer renderer)
+    {
+        if (renderer.color != player.color)
+        {
+            renderer.color = player.color;
+        }
+    }
+
+    private void updateMoveByForce(MoveByForce move)
+    {
+        move.horizontalAxis = player.inputHorizontalAxis;
+        move.verticalAxis = player.inputVerticalAxis;
+    }
+}
