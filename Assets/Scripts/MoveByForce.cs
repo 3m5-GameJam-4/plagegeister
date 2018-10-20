0547e2aa64037e360dfb7ea47258c6816a89da40
Gilbert Röhrbein <gilbert@ifsr.de>
2018-10-20 12:27:53 +0200

    add Swarm prefab, MoveByForce script

---
+++ Assets/Scripts/MoveByForce.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByForce : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float thrust = 1f;
+
+    void Start()
+    {
+        if (!rb) rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        var hori = Input.GetAxis("Horizontal");
+        var vert = Input.GetAxis("Vertical");
+
+        var direction = new Vector2(hori, vert);
+        
+        rb.AddForce(direction * thrust, ForceMode2D.Impulse);
+    }
+
+    public void Foo()
+    {
+        Debug.Log("XXX");
+    }
+}
