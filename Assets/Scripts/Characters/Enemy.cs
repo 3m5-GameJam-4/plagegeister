e06591d497c1cb054a05dae7c152ae9a9fa2ef72
Lars Wobus <lars.wobus@web.de>
2018-10-21 10:39:42 +0200

    Enemy moves to target

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
+	[SerializeField] private Transform _target;
+	[SerializeField] private float _speed = 5;
 
-	// Use this for initialization
-	void Start () {
-		
+	public Transform Target
+	{
+		get { return _target; }
+		set { _target = value; }
 	}
+
+	public float Speed => _speed;
 	
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update ()
+	{
+		var target = Target;
+		if (target == null)
+		{
+			return;
+		}
+
+		MoveTowardsTarget(transform.position, target.position, Speed * Time.deltaTime);
+	}
+
+	private void MoveTowardsTarget(Vector3 current, Vector3 target, float maxDistanceDelta)
+	{
+		transform.position = Vector3.MoveTowards(current, target, maxDistanceDelta);
 	}
 }
