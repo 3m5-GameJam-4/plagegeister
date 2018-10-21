ae6d401f29d73879b96771a4cf050da766bb7ffa
Steve Gotzel <steve.gotzel@3m5.de>
2018-10-21 10:03:50 +0200

    Add Assets Add Interface

---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IAudio {
+    void PlayAudio(string type);
+    // list of strings:
+    // attackEnemy
+    // attackSlime
+    // dieSwarm
+    // dieEnemy
+}
