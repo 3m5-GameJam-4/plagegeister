b5174e5db97eee08f4dc2177455158c7b664321f
Lars Wobus <lars.wobus@web.de>
2018-10-21 13:42:03 +0200

    Enemies follow nearest player

--- Assets/Scripts/Characters/EnemyRegistry.cs
+++ Assets/Scripts/Characters/EnemyRegistry.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyRegistry : MonoBehaviour
@@ -6,8 +7,58 @@ public class EnemyRegistry : MonoBehaviour
 	[SerializeField] private Transform _target;
 	[SerializeField] private float _reactionTime = 1.0f;
 	private List<Enemy> _enemies = new List<Enemy>();
+	[SerializeField] private List<Player> _players = new List<Player>();
 	private Random _random;
 
+	private void Start()
+	{
+		_random = new Random();
+		InvokeRepeating(nameof(RecalculateAnger), 0, _reactionTime);
+	}
+
+	private void RecalculateAnger()
+	{
+		foreach (var enemy in _enemies)
+		{
+			var nearestPlayer = FindNearestPlayer(enemy);
+			if (nearestPlayer == null)
+			{
+				continue;
+			}
+			
+			TraceTargetOnSight(enemy, nearestPlayer);
+			if (enemy.Target == null)
+			{
+				TraceTargetOnAnger(enemy, nearestPlayer);
+			}
+		}
+	}
+
+	private Player FindNearestPlayer(Enemy enemy)
+	{	
+		var player = _players.Select(element => new { element, Distance = Vector3.Distance(element.transform.position, enemy.transform.position) })
+			.OrderBy(element => element.Distance)
+			.FirstOrDefault();
+
+		return player?.element;
+	}
+
+	private void TraceTargetOnSight(Enemy enemy, Player player)
+	{
+		var sightDistance = enemy.SightDistance;
+		var distanceToNearestTarget = Vector3.Distance(player.transform.position, enemy.transform.position);
+
+		enemy.Target = (distanceToNearestTarget < sightDistance)? player.transform : null;
+	}
+
+	private void TraceTargetOnAnger(Enemy enemy, Player player)
+	{
+		var anger = enemy.Anger;
+		var value = Random.Range(0.0f, 1.0f);
+		
+		enemy.Target = (value < anger)? player.transform : null;
+	}
+	
 	public void RegisterEnemy(Enemy enemy)
 	{
 		if (_enemies.Contains(enemy))
@@ -23,37 +74,18 @@ public class EnemyRegistry : MonoBehaviour
 		_enemies.Remove(enemy);
 	}
 
-	private void Start()
+	public void RegisterPlayer(Player player)
 	{
-		_random = new Random();
-		InvokeRepeating(nameof(RecalculateAnger), 0, _reactionTime);
-	}
-
-	private void RecalculateAnger()
-	{
-		foreach (var enemy in _enemies)
+		if (_players.Contains(player))
 		{
-			TraceEnemyOnSight(enemy);
-			if (enemy.Target == null)
-			{
-				TraceEnemyOnAnger(enemy);
-			}
+			return;
 		}
-	}
-
-	private void TraceEnemyOnSight(Enemy enemy)
-	{
-		var sightDistance = enemy.SightDistance;
-		var distanceToNearestTarget = Vector3.Distance(_target.position, enemy.transform.position);
-
-		enemy.Target = (distanceToNearestTarget < sightDistance)? _target : null;
-	}
-
-	private void TraceEnemyOnAnger(Enemy enemy)
-	{
-		var anger = enemy.Anger;
-		var value = Random.Range(0.0f, 1.0f);
 		
-		enemy.Target = (value < anger)? _target : null;
+		_players.Add(player);
+	}
+	
+	public void UnRegisterPlayer(Player player)
+	{
+		_players.Remove(player);
 	}
 }
