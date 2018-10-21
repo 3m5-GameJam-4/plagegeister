17b374c5036e307c11c56848029c9ffefc0da2eb
Steve Gotzel <steve.gotzel@3m5.de>
2018-10-21 19:16:01 +0200

    Update

--- Assets/Scripts/MultiDisplay.cs
+++ Assets/Scripts/MultiDisplay.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MultiDisplay : MonoBehaviour {
     // Use this for initialization
     void Start() {
+        Debug.Log("displays connected: " + Display.displays.Length);
         for (int i = 0; i < Display.displays.Length; i++) {
             Display.displays[i].Activate();
         }   
